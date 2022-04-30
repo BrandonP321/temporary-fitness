6b6f405cddfd71713572d3ae708127ee0b95bdde
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-04-29 20:46:16 -0700

    Removed console.log()'s

--- packages/web/src/Areas/AreaOne/AreaOne.tsx
+++ packages/web/src/Areas/AreaOne/AreaOne.tsx
@@ -5,8 +5,7 @@ import { RouteDefs } from "../../global/Navigation/RouteDefs";
 const Pages = RouteDefs.Areas.AreaOne.pages
 
 export default function AreaOne() {
-	console.log("areaone");
-	console.log(Pages.PageOne.partialPath)
+
 	return (
 		<Routes>
 			<Route path={Pages.PageOne.partialPath} element={<h1/>}/>
