e435c29d36a0bdfcb111a7a986e3a9c29f9aaabe
Brandon Phillips <brandon.phillips@bphillips.dev>
2022-05-03 18:08:05 -0700

    Added test buttons for analytics tracking

--- packages/web/src/react-app-env.d.ts
+++ packages/web/src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+import "react";
+
+declare module "react" {
+	// custom attributes for all react intrinsic elements
+	interface HTMLAttributes<T> {
+		/* analytics id for GTM tracking */
+		"data-analytics-id"?: string;
+	}
+}
